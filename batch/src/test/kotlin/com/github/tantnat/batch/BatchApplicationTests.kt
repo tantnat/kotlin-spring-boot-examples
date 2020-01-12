d52c7c45874cb3a1260d6aee7a6fb33a1b32cde0
vitalii.zagorskii <zagorskyvs@gmail.com>
2020-01-12 19:48:33 +0300

    Spring Batch Initial Commit

---
+++ batch/src/test/kotlin/com/github/tantnat/batch/BatchApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.github.tantnat.batch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BatchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
