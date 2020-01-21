95178ad8ca1902f5c18a1cbe0b1c5a7f704cb6d8
tantat <zagorskyvs@gmail.com>
2020-01-21 21:39:51 +0300

    Initial commit

---
+++ security/src/test/kotlin/com/github/tantnat/security/SecurityApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.github.tantnat.security
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
